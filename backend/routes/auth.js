5595d1821e7773260aadc07afbae140f8a8e88a5
yusuf atesoglu <93738891+YusufAtesoglu@users.noreply.github.com>
2024-07-29 13:12:45 +0300

    Create user model

---
+++ backend/routes/auth.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/User.js");
+
+const generateRandomAvatar = () => {
+    const randomAvatar = Math.floor(Math.random() * 71);
+    return `https://i.pravatar.cc/300?img=${randomAvatar}`;
+  };
+ 
+//Kullanıcı oluşturma (Creat-Register)
+router.post("/register", async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      const defaultAvatar=generateRandomAvatar();
+const existingUser= await User.findOne({email})
+
+if(existingUser){
+    return res.status(400).json({error:"Email is already registed."})
+}
+
+  const hashedPassword=await bcrypt.hash(password,10);
+
+      const newUser = await new User({
+        username,
+        email,
+        password:hashedPassword,
+        avatar:defaultAvatar,
+      });
+  
+      await newUser.save();
+  
+      res.status(201).json(newUser);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Server error." });
+    }
+  });
+
+module.exports=router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 //Diğer rota dosyalarını içe aktarıyoruz
 const productRoute=require("./products.js");
+const authRoute=require("./auth.js");
 const categoryRoute=require("./categories.js");
 
 //Her rotayı ilgili yol altında kullanıyoruz
 router.use("/categories",categoryRoute);
+router.use("/auth",authRoute);
 router.use("/products",productRoute);
 
 module.exports=router;
